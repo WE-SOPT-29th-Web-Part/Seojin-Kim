71dffe2a0ec49802727cbc397325363939e41827
seojin kim <001106ksj@gmail.com>
2021-11-12 23:46:15 +0900

    feat: 중첩 라우팅과 이쁜 애니메이션
    * svg를 안 쓰고 keyframes로.. 미안해요ㅜㅜ

--- 4-velog/src/pages/home/index.tsx
+++ 4-velog/src/pages/home/index.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
-import { Route, useRouteMatch } from 'react-router';
+import React, { useState } from 'react';
+import { Redirect, Route, useRouteMatch } from 'react-router';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import UserProfile from '../../components/UserProfile';
 import PostingPage from './postings';
 import SeriesPage from './series';
-import { StyledContainer, StyledWrapper } from './style';
+import {
+  GreenLine,
+  LinkContainer,
+  LinkWrapper,
+  StyledContainer,
+  StyledWrapper,
+} from './style';
 
 export default function HomePage() {
   const match = useRouteMatch();
+
+  const [isPostSelected, setIsPostSelected] = useState(true);
+
   return (
     <StyledWrapper>
       <StyledContainer>
         <Header />
         <UserProfile />
-        <Link to={`${match.path}/postings`}>포스팅</Link>
-        <Link to={`${match.path}/series`}>시리즈</Link>
+        <LinkContainer>
+          <LinkWrapper focused={isPostSelected}>
+            <Link
+              to={`${match.path}/postings`}
+              onClick={() => {
+                setIsPostSelected(true);
+              }}
+            >
+              글
+            </Link>
+          </LinkWrapper>
+          <LinkWrapper focused={!isPostSelected}>
+            <Link
+              to={`${match.path}/series`}
+              onClick={() => {
+                setIsPostSelected(false);
+              }}
+            >
+              시리즈
+            </Link>
+          </LinkWrapper>
+          <GreenLine isPostSelected={isPostSelected} />
+        </LinkContainer>
         <Route path={`${match.path}/postings`} component={PostingPage} />
         <Route path={`${match.path}/series`} component={SeriesPage} />
+        <Route path={`${match.path}/`}>
+          <Redirect to={`${match.path}/postings`} />
+        </Route>
       </StyledContainer>
     </StyledWrapper>
   );
--- 4-velog/src/pages/home/style.ts
+++ 4-velog/src/pages/home/style.ts
@@ -11,4 +11,65 @@ const StyledContainer = styled.div`
   overflow-x: hidden;
 `;
 
-export { StyledWrapper, StyledContainer };
+const LinkContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  position: relative;
+  gap: 24px;
+  margin: 0 calc(50% - 144px);
+  padding: 24px 0;
+  text-align: center;
+`;
+
+interface ILink {
+  focused: boolean;
+}
+const LinkWrapper = styled.div<ILink>`
+  & > a {
+    font-size: 1.2rem;
+    text-decoration: none;
+    text-align: center;
+    color: ${(props) => (props.focused ? 'rgb(12, 166, 120)' : 'black')};
+  }
+`;
+
+interface IGreenLine {
+  isPostSelected: boolean;
+}
+
+const GreenLine = styled.div<IGreenLine>`
+  width: 132px;
+  height: 2px;
+  background-color: rgb(12, 166, 120);
+  position: absolute;
+  top: 60px;
+  animation: ${(props) => (props.isPostSelected ? 'moveLeft' : 'moveRight')}
+    0.6s forwards;
+
+  @keyframes moveLeft {
+    from {
+      right: 0;
+    }
+    to {
+      right: 156px;
+    }
+  }
+
+  @keyframes moveRight {
+    from {
+      left: 0;
+    }
+    to {
+      left: 156px;
+    }
+  }
+`;
+
+export {
+  StyledWrapper,
+  StyledContainer,
+  LinkContainer,
+  LinkWrapper,
+  GreenLine,
+};
